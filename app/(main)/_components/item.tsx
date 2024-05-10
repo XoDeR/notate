1dcb3c5ec8732e00616b80088453e13c1b489298
Volodymyr <XoDeR@users.noreply.github.com>
2024-05-10 20:26:09 +0200

    publish popover component

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -116,7 +116,7 @@ const Item = ({
       {documentIcon ? (
         <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
       ) : (
-        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground font-medium" />
+        <Icon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground font-medium" />
       )}
       <span className="truncate">{label}</span>
       {isSearch && (
